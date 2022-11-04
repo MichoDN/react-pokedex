fb69466f585268548dc02738f73a0cdf497ed188
MichoDN <michoproyect72@gmail.com>
2022-11-03 23:14:57 -0400

    Mayusc in import fixed

--- src/store/index.js
+++ src/store/index.js
@@ -1,7 +1,7 @@
 import { configureStore } from '@reduxjs/toolkit'
 import currentPageSlice from './slices/currentPage.slice'
 import selectedListSlice from './slices/selectedList.slice'
-import selectedTypeSlice from './slices/SelectedType.slice'
+import selectedTypeSlice from './slices/selectedType.slice'
 import postPerPageSlice from './slices/postPerPage.slice'
 import userNameSlice from './slices/userName.slice'
 import currentSearchSlice from './slices/currentSearch.slice'
